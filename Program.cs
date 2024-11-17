50c146e38265262a49aba2c434dbdcecf4c98a97
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 21:47:23 +0100

    Closes #3 - Solució exercici 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static bool Negatiu(int num)
+        {
+            return num < 0;
+        }
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("Introdueix un numero");
+
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (Negatiu(num))
+                {
+                    Console.WriteLine("El numero si es negatiu");
+                }
+                else
+                {
+                    Console.WriteLine("El numero no es negatiu");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
