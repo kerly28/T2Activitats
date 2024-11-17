e976e3dd789aeb2df3aa40289bc089dd89d5cd6f
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 22:05:59 +0100

    Closes #4 - Solució exercici 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: L'usuari introdueix un número
+using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static bool PositiveRank(int num)
+        {
+            return (num > 0 && num < 100);
+        }
+
+        public static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Introdueix un número:");
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (PositiveRank(num))
+                {
+                    Console.WriteLine("El numero es positiu i menor a 100");
+                }
+                else
+                {
+                    Console.WriteLine("El numero no es un nombre vàlid");
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error");
+
+            }
+        }
+    }
+}
+//POST: El programa retorna si el número és positiu i menor a 100
