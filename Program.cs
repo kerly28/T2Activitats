8de570b63cef6e89a38269b94b3846bd07f9d7ad
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 20:58:37 +0100

    exercici 1 Closes #exercici1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static bool Natural(int numUser)
+        {
+            return (numUser >= 0);
+        }
+        public static void Main(string[] args)
+        {
+            int numUser;
+            Console.WriteLine("Introdueix un numero:");
+            try
+            {
+                numUser = Convert.ToInt32(Console.ReadLine());
+                if (Natural(numUser))
+                {
+                    Console.WriteLine("Es natural");
+                }
+                else
+                {
+                    Console.WriteLine("No es natural");
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
