1d1f6906d8b8806b8e4389fa1ad2ee88cfdefb89
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 22:10:04 +0100

    Closes #5  - Solució exercici 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: L'usuari introdueix dos valors enters
+using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static void Main(string[] args)
+
+        {
+            Console.WriteLine("Introdueix el primer valor:");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introdueix el segon valor:");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Swap(ref valor1, ref valor2);
+            Console.WriteLine($"Valors intercanviats: valor1= {valor1}, valor2= {valor2}");
+        }
+    }
+}
+//POST: El programa intercanvia els valors introduits per l'usuari
