aa2e1fe89d20036203f8d14b76227d11457948a5
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 22:28:57 +0100

    Closes #6 - Solució exercici 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: L'usuari introdueix un número
+
+using system;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static double ValorAbsolut(double usernum)
+        {
+            if (usernum < 0)
+            {
+                usernum = -usernum;
+            }
+            return usernum;
+        }
+
+        public static void Main()
+        {
+
+            Console.WriteLine("Introdueix un número:");
+            double usernum = Convert.ToDouble(Console.ReadLine());
+            double resultat = ValorAbsolut(usernum);
+            Console.WriteLine($"El valor absolut de {usernum} és {resultat}");
+        }
+    }
+}
+//POST: El programa retorna el valor absolut del número introduït per l'usuari
