ea4e64340bb5dd84003769eb28eab23079a12ec1
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-17 21:36:51 +0100

    Solucio ac2 Closes #2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// PRE: l'usuari introdueix un nombre enter y una potència
+
+using System;
+
+namespace Metode
+{
+    public class Program
+    {
+
+        public static int Potencia(int num, int exp)
+        {
+            int result = 1;
+            for (int i = 0; i < exp; i++)
+            {
+                result *= num;
+            }
+            return result;
+        }
+
+        public static void Main()
+        {
+            int num, exp;
+            Console.WriteLine("Introdueix un numero:");
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Introdueix la potencia:");
+                exp = Convert.ToInt32(Console.ReadLine());
+
+                int result = Potencia(num, exp);
+                Console.WriteLine($"El resultat de {num} elevat a la potencia {exp} és: {result}");
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("S'ha produït un error.");
+            }
+        }
+    }
+}
+// PRECON: es mostra per pantalla el resultat de la potència del nombre introduït per l'usuari
