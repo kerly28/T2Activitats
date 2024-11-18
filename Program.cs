0743cf58ab1eae7ba14d29518e49fac4eaf366a8
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-18 15:42:01 +0100

    Closes #10 - Solució exercici 10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: L'usuari introdueix un any
+using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static bool Traspas(int age)
+        {
+            return age % 4 == 0;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Introdueix un any:");
+            int useryear;
+            useryear = Convert.ToInt32(Console.ReadLine());
+            if (Traspas(useryear))
+            {
+                Console.WriteLine("És un any de traspàs");
+            }
+            else
+            {
+                Console.WriteLine("No és un any de traspàs");
+            }
+        }
+    }
+}
+//POST: L'any introduït s'informa si és any de traspàs o no
