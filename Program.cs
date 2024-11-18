bd020c93c669d66b07346ccf5aae1c1d9a4e4f80
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-18 17:05:02 +0100

    Closes #12 - Solució exercici 12

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: S'introdueix graus (serà interpretat com a Celsius)
+using System;
+namespace Metode
+{
+    public class Program
+    {
+        public static int ConvertirCelsiusAFahrenheit(int celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+        public static void Main()
+        {
+            int celsius;
+            Console.WriteLine("Introdueix els graus Celsius:");
+            try
+            {
+                celsius = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Els graus en Fahrenheit són: " + ConvertirCelsiusAFahrenheit(celsius));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
+//POST: El programa converteix els graus Celsius introduits en Fahrenheit
