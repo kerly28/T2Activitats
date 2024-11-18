c54917501ace308960b0b5b752a2a5330700f554
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-18 11:30:37 +0100

    Closes #7 - Solució exercici 7

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//PRE: L'usuari introdueix una frase
+
+using System;
+
+namespace Metode
+{
+    public class Program
+    {
+        public static string Upper(string s)
+        {
+            return s.ToUpper();
+
+        }
+        public static string Lower(string s)
+        {
+            return s.ToLower();
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introdueix una frase:");
+            string user = Console.ReadLine();
+
+            if (user == user.ToUpper())
+            {
+                Console.WriteLine($"Retorn en minúscules: {user.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine($"Retorn en majúscules: {user.ToUpper()}");
+            }
+
+
+        }
+    }
+
+}
+//POST: El programa retorna la frase en majúscules si l'usuari la introdueix en minúscules i viceversa
